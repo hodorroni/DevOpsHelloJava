91a7cf9ec6007bbfbcccb35b6400dff9a205f09d
hodorroni <hodorroni@gmail.com>
2024-05-20 10:36:48 +0300

    just testing after job

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 public class Hello{
 public static void main(String[] args) {
 for(int i=0;i<=10;i++){
-System.out.println("Hello World..."+i);
+System.out.println("Hello World...."+i);
 }
-System.out.println("This is changes made and pushing");
 }
 }
